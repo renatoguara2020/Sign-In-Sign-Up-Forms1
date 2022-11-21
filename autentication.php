5983cffa9a70fb9fc5530d6de36a3cae9b411a4e
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-21 15:15:54 -0300

    OK

--- autentication.php
+++ autentication.php
@@ -1,17 +1,17 @@
 <?php
 
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
+        if (!isset($_SERVER['PHP_AUTH_USER']) && (!isset($_SERVER['PHP_AUTH_PW']))) {
+            header('WWW-Authenticate: Basic realm="My Realm"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo 'Text to send if user hits Cancel button';
+            exit;
+        } else {
+            echo "<p>Hello {$_SERVER['PHP_AUTH_USER']} Welcome !!!!!!.</p>";
+            echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+        }
 
-if (!isset($_SERVER['PHP_AUTH_USER']) && (!isset($_SERVER['PHP_AUTH_PW']))) {
-    header('WWW-Authenticate: Basic realm="My Realm"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo 'Text to send if user hits Cancel button';
-    exit;
-} else {
-    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
-    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
 }
-
-
 ?>
